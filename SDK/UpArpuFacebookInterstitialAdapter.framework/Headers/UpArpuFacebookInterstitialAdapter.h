91b1996717e0efa519aeccab1d23f2fbd8001e05
huangxu_liu <huangxu_liu@mobvista.com>
2018-10-15 10:12:55 +0800

    facebook adapters

---
+++ SDK/UpArpuFacebookInterstitialAdapter.framework/Headers/UpArpuFacebookInterstitialAdapter.h
@@ -0,0 +1,19 @@
+//
+//  UpArpuFacebookInterstitialAdapter.h
+//  UpArpuFacebookInterstitialAdapter
+//
+//  Created by Martin Lau on 29/09/2018.
+//  Copyright © 2018 Martin Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UpArpuFacebookInterstitialAdapter.
+FOUNDATION_EXPORT double UpArpuFacebookInterstitialAdapterVersionNumber;
+
+//! Project version string for UpArpuFacebookInterstitialAdapter.
+FOUNDATION_EXPORT const unsigned char UpArpuFacebookInterstitialAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UpArpuFacebookInterstitialAdapter/PublicHeader.h>
+
+

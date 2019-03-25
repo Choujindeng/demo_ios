f606b9c0a41c81d11e78ed098669f9c1bb1fa502
张健明 <253521624@qq.com>
2019-03-25 15:58:59 +0800

    v3.5.0

--- SDK/UpArpuSDK.framework/Headers/UPArpuAPI.h
+++ SDK/UpArpuSDK.framework/Headers/UPArpuAPI.h
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+extern NSString *const UPADShowingErrorDomain;
 
 extern NSString *const UPADLoadingErrorDomain;
 extern NSInteger const UPADLoadingErrorCodePlacementStrategyInvalidResponse;

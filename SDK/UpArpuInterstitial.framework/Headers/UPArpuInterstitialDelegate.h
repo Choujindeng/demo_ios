4900fd6987d4379eb79afa159aa88492e2eb187d
张健明 <253521624@qq.com>
2019-08-19 20:30:13 +0800

    v4.2.1

--- SDK/UpArpuInterstitial.framework/Headers/UPArpuInterstitialDelegate.h
+++ SDK/UpArpuInterstitial.framework/Headers/UPArpuInterstitialDelegate.h
@@ -9,14 +9,25 @@
 #ifndef UPArpuInterstitialDelegate_h
 #define UPArpuInterstitialDelegate_h
 #import <UpArpuSDK/UpArpuSDK.h>
+
+extern NSString *const kUPArpuInterstitialDelegateExtraNetworkIDKey;
+extern NSString *const kUPArpuInterstitialDelegateExtraAdSourceIDKey;
 @protocol UPArpuInterstitialDelegate<UPArpuAdLoadingDelegate>
--(void) interstitialDidShowForPlacementID:(NSString*)placementID;
--(void) interstitialFailedToShowForPlacementID:(NSString*)placementID error:(NSError*)error;
--(void) interstitialDidStartPlayingVideoForPlacementID:(NSString*)placementID;
--(void) interstitialDidEndPlayingVideoForPlacementID:(NSString*)placementID;
--(void) interstitialDidFailToPlayVideoForPlacementID:(NSString*)placementID error:(NSError*)error;
--(void) interstitialDidCloseForPlacementID:(NSString*)placementID;
--(void) interstitialDidClickForPlacementID:(NSString*)placementID;
+-(void) interstitialDidShowForPlacementID:(NSString*)placementID DEPRECATED_ATTRIBUTE;
+-(void) interstitialFailedToShowForPlacementID:(NSString*)placementID error:(NSError*)error DEPRECATED_ATTRIBUTE;
+-(void) interstitialDidStartPlayingVideoForPlacementID:(NSString*)placementID DEPRECATED_ATTRIBUTE;
+-(void) interstitialDidEndPlayingVideoForPlacementID:(NSString*)placementID DEPRECATED_ATTRIBUTE;
+-(void) interstitialDidFailToPlayVideoForPlacementID:(NSString*)placementID error:(NSError*)error DEPRECATED_ATTRIBUTE;
+-(void) interstitialDidCloseForPlacementID:(NSString*)placementID DEPRECATED_ATTRIBUTE;
+-(void) interstitialDidClickForPlacementID:(NSString*)placementID DEPRECATED_ATTRIBUTE;
+
+-(void) interstitialDidShowForPlacementID:(NSString*)placementID extra:(NSDictionary*)extra;
+-(void) interstitialFailedToShowForPlacementID:(NSString*)placementID error:(NSError*)error extra:(NSDictionary*)extra;
+-(void) interstitialDidStartPlayingVideoForPlacementID:(NSString*)placementID extra:(NSDictionary*)extra;
+-(void) interstitialDidEndPlayingVideoForPlacementID:(NSString*)placementID extra:(NSDictionary*)extra;
+-(void) interstitialDidFailToPlayVideoForPlacementID:(NSString*)placementID error:(NSError*)error extra:(NSDictionary*)extra;
+-(void) interstitialDidCloseForPlacementID:(NSString*)placementID extra:(NSDictionary*)extra;
+-(void) interstitialDidClickForPlacementID:(NSString*)placementID extra:(NSDictionary*)extra;
 @end
 
 #endif /* UPArpuInterstitialDelegate_h */

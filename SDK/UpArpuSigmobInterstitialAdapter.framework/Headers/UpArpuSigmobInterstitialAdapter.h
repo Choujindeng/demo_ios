f683a039ab35acab85c2cd4d514028006d0fe8bf
张健明 <253521624@qq.com>
2019-09-11 17:57:59 +0800

    sigmob adapter

---
+++ SDK/UpArpuSigmobInterstitialAdapter.framework/Headers/UpArpuSigmobInterstitialAdapter.h
@@ -0,0 +1,19 @@
+//
+//  UpArpuSigmobInterstitialAdapter.h
+//  UpArpuSigmobInterstitialAdapter
+//
+//  Created by Martin Lau on 2019/9/9.
+//  Copyright © 2019 Martin Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UpArpuSigmobInterstitialAdapter.
+FOUNDATION_EXPORT double UpArpuSigmobInterstitialAdapterVersionNumber;
+
+//! Project version string for UpArpuSigmobInterstitialAdapter.
+FOUNDATION_EXPORT const unsigned char UpArpuSigmobInterstitialAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UpArpuSigmobInterstitialAdapter/PublicHeader.h>
+
+

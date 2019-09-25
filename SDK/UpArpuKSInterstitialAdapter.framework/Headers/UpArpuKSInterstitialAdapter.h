b75c46ba749cf6fb07071d3a61a60837319beff2
张健明 <253521624@qq.com>
2019-09-25 20:12:14 +0800

    v4.4.2 framework

---
+++ SDK/UpArpuKSInterstitialAdapter.framework/Headers/UpArpuKSInterstitialAdapter.h
@@ -0,0 +1,19 @@
+//
+//  UpArpuKSInterstitialAdapter.h
+//  UpArpuKSInterstitialAdapter
+//
+//  Created by Topon on 2019/9/16.
+//  Copyright © 2019 Martin Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UpArpuKSInterstitialAdapter.
+FOUNDATION_EXPORT double UpArpuKSInterstitialAdapterVersionNumber;
+
+//! Project version string for UpArpuKSInterstitialAdapter.
+FOUNDATION_EXPORT const unsigned char UpArpuKSInterstitialAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UpArpuKSInterstitialAdapter/PublicHeader.h>
+
+
---
+++ SDK/UpArpuKSRewardedVideoAdapter.framework/Headers/UpArpuKSRewardedVideoAdapter.h
@@ -0,0 +1,19 @@
+//
+//  UpArpuKSRewardedVideoAdapter.h
+//  UpArpuKSRewardedVideoAdapter
+//
+//  Created by Topon on 2019/9/16.
+//  Copyright © 2019 Martin Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UpArpuKSRewardedVideoAdapter.
+FOUNDATION_EXPORT double UpArpuKSRewardedVideoAdapterVersionNumber;
+
+//! Project version string for UpArpuKSRewardedVideoAdapter.
+FOUNDATION_EXPORT const unsigned char UpArpuKSRewardedVideoAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UpArpuKSRewardedVideoAdapter/PublicHeader.h>
+
+

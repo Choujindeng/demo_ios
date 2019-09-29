f09f82add8e534ed374b396b2cb935ac94459948
张健明 <253521624@qq.com>
2019-09-29 17:00:46 +0800

    v4.4.3 更新第三方SDK

--- SDK/MTGSDKInterstitialVideo.framework/Versions/A/Headers/MTGInterstitialVideoAd.h
+++ SDK/MTGSDKInterstitialVideo.framework/Versions/A/Headers/MTGInterstitialVideoAd.h
@@ -8,7 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-#define MTGInterstitialVideoSDKVersion @"5.7.0"
+#define MTGInterstitialVideoSDKVersion @"5.8.0"
+
 
 @class MTGInterstitialVideoAdManager;
 @class MTGBidInterstitialVideoAdManager;

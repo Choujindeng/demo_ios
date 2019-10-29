ea8280c18f23c788259ddca098dd8a00620159c2
张健明 <253521624@qq.com>
2019-10-29 11:07:31 +0800

    v4.5.0

---
+++ SDK/UpArpuMyOffer.framework/Headers/UpArpuMyOffer.h
@@ -0,0 +1,19 @@
+//
+//  UpArpuMyOffer.h
+//  UpArpuMyOffer
+//
+//  Created by Martin Lau on 2019/9/23.
+//  Copyright © 2019 Martin Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UpArpuMyOffer.
+FOUNDATION_EXPORT double UpArpuMyOfferVersionNumber;
+
+//! Project version string for UpArpuMyOffer.
+FOUNDATION_EXPORT const unsigned char UpArpuMyOfferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UpArpuMyOffer/PublicHeader.h>
+
+

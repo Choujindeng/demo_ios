3e9ec5fb822ad09bc9e5e35714c1f3ffbb14a3a4
张健明 <253521624@qq.com>
2019-11-11 14:04:17 +0800

    add sdk

--- AnyThinkSDKDemo/SDK/MTGSDKBidding.framework/Versions/A/Headers/MTGBiddingSDK.h
+++ AnyThinkSDKDemo/SDK/MTGSDKBidding.framework/Versions/A/Headers/MTGBiddingSDK.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define MTGBiddingSDKVersion @"5.7.0"
+#define MTGBiddingSDKVersion @"5.8.0"
 
 @interface MTGBiddingSDK : NSObject
 
--- AnyThinkSDKDemo/SDK/MTGSDKReward.framework/Versions/A/Headers/MTGRewardAd.h
+++ AnyThinkSDKDemo/SDK/MTGSDKReward.framework/Versions/A/Headers/MTGRewardAd.h
@@ -9,7 +9,8 @@
 #import <Foundation/Foundation.h>
 #import <MTGSDK/MTGRewardAdInfo.h>
 
-#define MTGRewardVideoSDKVersion @"5.7.0"
+#define MTGRewardVideoSDKVersion @"5.8.0"
+
 
 /**
  *  This protocol defines a listener for ad video load events.
